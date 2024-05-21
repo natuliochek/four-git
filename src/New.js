8840a37ffe8ac943855876546171e693817be60a
natuliochek <natvicor@mail.ru>
2024-05-21 16:05:36 +0200

    modified App css and created new file

---
+++ src/New.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='main'>
+      <h1>Hello, GitHub waits for us!</h1>
+      <h2>Learn REACT with Alla !!!</h2>
+    </div>
+  );
+}
+
+export default App;
